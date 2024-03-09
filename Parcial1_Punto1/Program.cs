bc83424225a892c6a3e39f4dbf260d7d6b5fedfc
harduqe <haroldarbelaezduque@gmail.com>
2024-03-08 21:16:45 -0500

    Solicitando sueldo, variables

---
+++ Parcial1_Punto1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Parcial1_Punto1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Solicitar el sueldo base al vendedor
+            Console.Write("Ingrese el sueldo base del vendedor: $");
+            double sueldoBase = Convert.ToDouble(Console.ReadLine());
+
+            // Inicializar variables
+            double objetivoVentas = 1000000;
+            double beneficioExtra = 100000;
+            double totalVentas = 0;
+            double mayorComision = 0;
+        }
+    }
+}
