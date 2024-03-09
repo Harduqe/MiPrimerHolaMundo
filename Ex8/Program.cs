dd0865c4c593d7a1237f2a2288dd4f7fd42f85ef
harduqe <haroldarbelaezduque@gmail.com>
2024-03-08 20:51:25 -0500

    first solution

---
+++ Ex8/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Ex8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Solicita al usuario que ingrese un número
+            Console.Write("Por favor, ingresa un número: ");
+
+            // Lee la línea de entrada del usuario y la convierte en un entero
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            // Verifica si el número es par o no
+            if (numero % 2 == 0)
+            {
+                Console.Write("El número ingresado es par.");
+            }
+            else
+            {
+                Console.Write("El número ingresado es impar.");
+            }
+
+            // Espera a que el usuario presione una tecla antes de cerrar
+            Console.Write("FINAL, FINAL, NO DA MAS...");
+           
+        }
+    }
+}
---
+++ Parcial1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Parcial1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Un tren sale de la estación “A” a las 10:00 AM y viaja a una velocidad constante de 80 km/h. Un segundo tren sale de la misma estación a las 11:00 AM y viaja en la misma dirección a una velocidad constante de 100 km / h. ¿A qué hora el segundo tren alcanzará al primer tren? Mostrar esta hora en pantalla
+
+            // Velocidades de los trenes en km/h
+
+            int v1 = 80; 
+            int v2 = 100; 
+
+            double t2 = CalcularTiempoAlcance(v1, v2); // Tiempo en horas que ha estado viajando el segundo tren
+        }
+    }
+}
